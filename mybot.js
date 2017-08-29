97eb0a8294072aa6415ad5fb8909f22c50daaa58
Atk <darkninjabuni@gmail.com>
2017-08-30 00:03:47 +0100

    First Decent Commit

--- mybot.js
+++ mybot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
-const replies = require("./textOnlyResponses.json");
+const replies = require("./cleanTextResponses.json");
 const fs = require("fs")
 
 // when bot finishes loading
@@ -12,28 +12,61 @@ client.on("ready", () => {
 
 // message is sent
 client.on("message", (message) => {
-  if (message.author.bot) return;
 
-  if (message.content.startsWith(config.prefix + "ping")) {
-    message.channel.send("pong!");
+  //checks for clean text responses and then if then terminates if message was from a bot or does not have a command prefix
+  if (replies[message.content]) {
+    message.channel.send(replies[message.content]);
+  } else if (message.author.bot || !message.content.startsWith(config.prefix)) {
+    return;
   }
+  const args = message.content.split(/\s+/g).slice(1)
 
+  //list emojis
   if (message.content.startsWith(config.prefix + "emojis")) {
     const emojiList = message.guild.emojis.map(e=>e.toString()).join(" ");
     message.channel.send(emojiList);
-    console.log("Triggered");
   }
 
-  if(replies[message.content]) {
-    message.channel.send(replies[message.content]);
+  // ASL lmao
+  if (message.content.startsWith(config.prefix + "asl")) {
+    let age = args[0];
+    let sex = args[1];
+    let location = args[2];
+    message.reply(` I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);
+  }
+
+  if (message.content.startsWith(config.prefix + "eval") && message.author.id === config.ownerID) {
+    try {
+      const code = args.join(" ");
+      let evaled = eval(code);
+
+      if (typeof evaled !== "string") {
+        evaled = require("util").inspect(evaled);
+      }
+
+      //message.channel.send(clean(evaled), {code:"x1"});
+      message.reply(` ran what you asked me to (I think):\n\`\`\`${code}\`\`\``);
+      //message.channel.send(clean(evaled));
+    } catch (err) {
+      message.channel.send(`\`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
   }
 });
 
 // new person joins server
 client.on("guildMemberAdd", (member) => {
-  member.guild.defaultChannel.send("$(member.user.username) has joined the server. Say hi!");
+  member.guild.defaultChannel.send(`$(member.user.username) has joined the server. Say hi!`);
   let baseRole = message.guild.roles.find("name", config.basicRole);
   member.addRole(baseRole).catch(console.error);
 });
+
+//shiny clean inputs :3
+function clean(text) {
+  if(typeof(text) === "string"){
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  } else {
+    return text;
+  }
+}
 
 client.login(config.token);
