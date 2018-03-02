97fc9989f5a00509d45caa2e14dea67f90ab9616
AtkLordOverAll <darkninjabuni@gmail.com>
2018-03-02 15:14:38 +0000

    permission rework, using tiered system (BE SURE TO UPDATE CONFIG)

--- mybot.js
+++ mybot.js
@@ -63,203 +63,224 @@ client.on("message", (message) => {
         command = msgArray[0];
     }
 
-    if (message.content.toLowerCase().startsWith("do your army impression")) {
-        let msg = message.content.slice(24).toUpperCase();
+    console.log(`Saw message with command ("${message.content}") sent from user ${message.author.username} (ID: ${message.author.id})`);
 
-        message.channel.send(`**Sir ${message.author.username}, yes, sir!**`);
+    if (message.member.roles.exists('id', config.tier1) || message.member.roles.exists('id', config.tier2) || message.member.roles.exists('id', config.tier3) || message.member.roles.exists('id', config.mods) || message.member.roles.exists('id', config.devs) || message.author.id == 212571213912866826) {
 
-        if (msg == "") {
+        // COMMANDS ANYONE CAN USE HERE
+
+        if (message.content.toLowerCase().startsWith("do your army impression")) {
+            let msg = message.content.slice(24).toUpperCase();
+
+            message.channel.send(`**Sir ${message.author.username}, yes, sir!**`);
+
+            if (msg == "") {
+                return;
+            }
+
+            message.delete(1000);
+
+            let out = "";
+            let sub = "";
+            let skip = false;
+
+            for (let ch = 0; ch < msg.length; ch++) {
+                sub = alphabet[msg.charAt(ch)];
+                skip = false;
+
+                if (sub != null) {
+                    out += sub;
+                } else {
+                    out += msg.charAt(ch);
+                    skip = true;
+                }
+
+                if (!skip) {
+                    out += addSpace(msg, ch);
+                }
+            }
+            message.channel.send(`${out}`);
             return;
         }
 
-        message.delete(1000);
+        // list emojis
+        /*if (command === "emojis") {
+            message.channel.send(message.guild.emojis.map(e=>e.toString()).join(" "));
+            return;
+        }*/
 
-        let out = "";
-        let sub = "";
-        let skip = false;
-
-        for (let ch = 0; ch < msg.length; ch++) {
-            sub = alphabet[msg.charAt(ch)];
-            skip = false;
-
-            if (sub != null) {
-                out += sub;
-            } else {
-                out += msg.charAt(ch);
-                skip = true;
+        if (command === "rgb" && msgArray.length == 3) {
+            let rgb = [0,0,0];
+            for (let a = 1; a < 4; a++) {
+                rgb[a] = (Math.floor(msgArray[a] / 85));
             }
 
-            if (!skip) {
-                out += addSpace(msg, ch);
+            let output = "Good question son!.\nAs you can clearly see, ";
+
+            if (rgb[0] === 0) {
+                output += "there isn't a lot of red, ";
+            } else if (rgb[0] === 1) {
+                output += "you've got some red in there, ";
+            } else if (rgb[0] > 1) {
+                output += "you've got a healthy dollop of red, ";
             }
-        }
-        message.channel.send(`${out}`);
-        return;
-    }
 
-    console.log(`Saw message with command ("${message.content}") sent from user ${message.author.username} (ID: ${message.author.id})`);
+            if (rgb[1] === 0) {
+                output += "not much green, ";
+            } else if (rgb[1] === 1) {
+                output += "you've got some green, ";
+            } else if (rgb[1] > 1) {
+                output += "a healthy dollop of Shrek in the mix, ";
+            }
 
-    // list emojis
-    /*if (command === "emojis") {
-        message.channel.send(message.guild.emojis.map(e=>e.toString()).join(" "));
-        return;
-    }*/
-
-    if (command === "rgb" && msgArray.length == 3) {
-        let rgb = [0,0,0];
-        for (let a = 1; a < 4; a++) {
-            rgb[a] = (Math.floor(msgArray[a] / 85));
-        }
-
-        let output = "Good question son!.\nAs you can clearly see, ";
-
-        if (rgb[0] === 0) {
-            output += "there isn't a lot of red, ";
-        } else if (rgb[0] === 1) {
-            output += "you've got some red in there, ";
-        } else if (rgb[0] > 1) {
-            output += "you've got a healthy dollop of red, ";
-        }
-
-        if (rgb[1] === 0) {
-            output += "not much green, ";
-        } else if (rgb[1] === 1) {
-            output += "you've got some green, ";
-        } else if (rgb[1] > 1) {
-            output += "a healthy dollop of Shrek in the mix, ";
-        }
-
-        if (rgb[2] === 0) {
-            output += "and that's about it to be honest.";
-        } else if (rgb[2] === 1) {
-            output += "and a splotch of blue to round it all off.";
-        } else if (rgb[2] > 1) {
-            output += "and holy shit this is my jam!\n*I'm blue da ba dee da ba daa*\n*Da ba dee da ba daa, da ba dee da ba daa, da ba dee da ba daa*\n*Da ba dee da ba daa, da ba dee da ba daa, da ba dee da ba daa*";
+            if (rgb[2] === 0) {
+                output += "and that's about it to be honest.";
+            } else if (rgb[2] === 1) {
+                output += "and a splotch of blue to round it all off.";
+            } else if (rgb[2] > 1) {
+                output += "and holy shit this is my jam!\n*I'm blue da ba dee da ba daa*\n*Da ba dee da ba daa, da ba dee da ba daa, da ba dee da ba daa*\n*Da ba dee da ba daa, da ba dee da ba daa, da ba dee da ba daa*";
+                message.channel.send(output);
+                return;
+            }
+            output += "\nI hope that helped you come to terms with that colour you just gave me. I've basically inherited the abilities of one of my many sons, Mallen.";
             message.channel.send(output);
             return;
         }
-        output += "\nI hope that helped you come to terms with that colour you just gave me. I've basically inherited the abilities of one of my many sons, Mallen.";
-        message.channel.send(output);
-        return;
-    }
 
-    if (command === "echo" && message.member.roles.find("name", "Bot Dev")) {
-        if (echoing) {
-            message.channel.send("Dad echo :(");
-            echoing = false;
-        } else {
-            message.channel.send("Dad echo!");
-            echoing = true;
-        }
-        return;
-    }
+    } else if (message.member.roles.exists('id', config.tier2) || message.member.roles.exists('id', config.tier3) || message.member.roles.exists('id', config.mods) || message.member.roles.exists('id', config.devs) || message.author.id == 212571213912866826) {
 
-    // list clean text responses (admin)
-    if (message.content.startsWith("list aliases")){
-        message.channel.send(`**Current aliases are:**\n${JSON.stringify(phrases).replace(/,/g, "\n").replace(/:/g,": ").replace(/{/g,"").replace(/}/g,"")}`);
-        return;
-    }
+        // COMMANDS FOR TIER 2 AND ABOVE USERS HERE
 
-    // add or suggest clean text responses
-    if (command === "alias" && message.member.roles.find("name", "Bot Dev")) {
-        //phrases[args[0]] = args[1];
-        let processThis = message.content.slice(command.length + 1).toLowerCase();
-        let speechMarkCount = 0;
-        if (processThis[0] != '"') {
-            return;
-        }
-        let alias = ["",""];
-        for (let i = 1; i < processThis.length; i++) {
-            if (processThis[i] == '"') {
-                speechMarkCount++;
-            } else if (speechMarkCount < 1) {
-                alias[0] += processThis[i];
-            } else if (speechMarkCount == 2) {
-                alias[1] += processThis[i];
+        // add clean text responses
+        if (command === "alias" && message.member.roles.find("name", "Bot Dev")) {
+            let processThis = message.content.slice(command.length + 1).toLowerCase();
+            let speechMarkCount = 0;
+            if (processThis[0] != '"') {
+                return;
             }
-            console.log(`"${alias[0]}": "${alias[1]}"`);
-        }
-        phrases[alias[0]] = alias[1];
-        saveJSON(phrases, "./cleanTextResponses.json");
-        message.channel.send("New hip and trendy phrase acquired. Watch out kiddo");
-    }
-
-    // remove clean text responses (admin)
-    if (command === "dealias" && message.member.roles.find("name", "Bot Dev")) {
-        let toRemove = message.content.slice(command.length + 1).match(/[^"]+/g);
-        if (phrases[toRemove]) {
-            message.channel.send(`I will no longer say "${phrases[toRemove]}" if you say "${toRemove}". Sorry for any offense caused?`);
-            delete phrases[toRemove];
+            let alias = ["",""];
+            for (let i = 1; i < processThis.length; i++) {
+                if (processThis[i] == '"') {
+                    speechMarkCount++;
+                } else if (speechMarkCount < 1) {
+                    alias[0] += processThis[i];
+                } else if (speechMarkCount == 2) {
+                    alias[1] += processThis[i];
+                }
+                console.log(`"${alias[0]}": "${alias[1]}"`);
+            }
+            phrases[alias[0]] = alias[1];
             saveJSON(phrases, "./cleanTextResponses.json");
-        } else {
-            message.channel.send("I didn't know I was meant to say something when you said that. Sorry.");
+            message.channel.send("New hip and trendy phrase acquired. Watch out kiddo");
         }
-        return;
-    }
 
-    // evalmsg (admin)
-    if (command === "evalmsg" && message.member.roles.find("name", "Bot Dev")) {
-        try {
-            const code = "message.channel.send(" + message.content.slice(command.length + 1) + ");";
-            console.log(code);
-            let evaled = eval(code);
+    } else if (message.member.roles.exists('id', config.tier3) || message.member.roles.exists('id', config.mods) || message.member.roles.exists('id', config.devs) || message.author.id == 212571213912866826) {
 
-            if (typeof evaled !== "string") {
-                evaled = require("util").inspect(evaled);
+        // COMMANDS FOR TIER 3 AND ABOVE USERS HERE
+
+        if (command === "echo" && message.member.roles.find("name", "Bot Dev")) {
+            if (echoing) {
+                message.channel.send("Dad echo :(");
+                echoing = false;
+            } else {
+                message.channel.send("Dad echo!");
+                echoing = true;
             }
-
-            message.reply(`I ran what you asked me to (I think):\n\`\`\`js\n${code}\`\`\``);
             return;
-        } catch (err) {
-            message.channel.send(`\`\`\`xl\n${clean(err)}\n\`\`\``);
         }
-    }
 
-    // set game (admin)
-    if (command === "setgame" && message.member.roles.find("name", "Bot Dev")) {
-        client.user.setPresence({game: {name: message.content.slice(command.length + 1), type: 0}});
-        return;
-    }
+    } else if (message.member.roles.exists('id', config.mods) || message.member.roles.exists('id', config.devs) || message.author.id == 212571213912866826) {
 
-    // eval (owner)
-    if (command === "eval" && message.author.id === config.ownerID) {
-        try {
-            const code = message.content.slice(command.length + 1);
-            let evaled = eval(code);
+        // COMMANDS FOR MODS HERE
 
-            if (typeof evaled !== "string") {
-                evaled = require("util").inspect(evaled);
-            }
-
-            //message.channel.send(clean(evaled), {code:"x1"});
-            message.reply(`I ran what you asked me to (I think):\n\`\`\`js\n${code}\`\`\``);
-            //message.channel.send(clean(evaled));
+        if (message.content.startsWith("set game to")) {
+            client.user.setPresence({game: {name: message.content.slice(command.length + 1), type: 0}});
             return;
-        } catch (err) {
-            message.channel.send(`\`\`\`xl\n${clean(err)}\n\`\`\``);
         }
-    }
 
-    if (message.content.startsWith("list role ids")) {
-        let roles = message.guild.roles.array();
-        let out = `**__Role IDs__:**\n`;
-        console.log(roles.length);
-        for (n = 0; n < roles.length; n++) {
-            if (roles[n].name != "@everyone") {
-                out += `**${roles[n].name}**: *${roles[n].id}*\n`;
+        // list clean text responses
+        if (message.content.startsWith("list aliases")){
+            message.channel.send(`**Current aliases are:**\n${JSON.stringify(phrases).replace(/,/g, "\n").replace(/:/g,": ").replace(/{/g,"").replace(/}/g,"")}`);
+            return;
+        }
+
+        // remove clean text responses
+        if (command === "dealias") {
+            let toRemove = message.content.slice(command.length + 1).match(/[^"]+/g);
+            if (phrases[toRemove]) {
+                message.channel.send(`I will no longer say "${phrases[toRemove]}" if you say "${toRemove}". Sorry for any offense caused?`);
+                delete phrases[toRemove];
+                saveJSON(phrases, "./cleanTextResponses.json");
+            } else {
+                message.channel.send("I didn't know I was meant to say something when you said that. Sorry.");
+            }
+            return;
+        }
+
+    } else if (message.member.roles.exists('id', config.devs) || message.author.id == 212571213912866826) {
+
+        // COMMANDS FOR DEVS HERE
+
+        if (message.content.startsWith("list role ids")) {
+            let roles = message.guild.roles.array();
+            let out = `**__Role IDs__:**\n`;
+            console.log(roles.length);
+            for (n = 0; n < roles.length; n++) {
+                if (roles[n].name != "@everyone") {
+                    out += `**${roles[n].name}**: *${roles[n].id}*\n`;
+                }
+            }
+            message.channel.send(out);
+            return;
+        }
+
+    } else if (message.author.id == 212571213912866826) {
+
+        // COMMANDS FOR BOT OWNER HERE
+
+        if (command === "eval") {
+            try {
+                const code = message.content.slice(command.length + 1);
+                let evaled = eval(code);
+
+                if (typeof evaled !== "string") {
+                    evaled = require("util").inspect(evaled);
+                }
+
+                //message.channel.send(clean(evaled), {code:"x1"});
+                message.reply(`I ran what you asked me to (I think):\n\`\`\`js\n${code}\`\`\``);
+                //message.channel.send(clean(evaled));
+                return;
+            } catch (err) {
+                message.channel.send(`\`\`\`xl\n${clean(err)}\n\`\`\``);
             }
         }
-        message.channel.send(out);
-        return;
+
+        if (command === "evalmsg") {
+            try {
+                const code = "message.channel.send(" + message.content.slice(command.length + 1) + ");";
+                console.log(code);
+                let evaled = eval(code);
+
+                if (typeof evaled !== "string") {
+                    evaled = require("util").inspect(evaled);
+                }
+
+                message.reply(`I ran what you asked me to (I think):\n\`\`\`js\n${code}\`\`\``);
+                return;
+            } catch (err) {
+                message.channel.send(`\`\`\`xl\n${clean(err)}\n\`\`\``);
+            }
+        }
+
     }
 });
 
 // new person joins server
 client.on("guildMemberAdd", (member) => {
     member.guild.defaultChannel.send(`Hi ${member.user.toString()} :3 Welcome to the server!`);
-    let baseRole = member.guild.roles.find("name", config.basicRole);
     console.log(`User ${member.user.username} (ID: ${member.user.id}) joined the server`);
-    member.addRole(baseRole).catch(console.error);
+    member.addRole(config.tier1).catch(console.error);
 });
 
 // used with eval
